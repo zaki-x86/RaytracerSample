66e84e68cee6a063b2528bb794de6ecffa314c47
Mohamed Zaki <mamadovzicx@gmail.com>
2023-05-06 16:27:24 +0300

    Rendering antialiased spheres
    - Added camera class
    - Created a hittable_list class to store multiple objects
    - created abstract class: hittable to be implemented by any object
    - Added sphere class
    - added <common.h>
    - added antialiasing by sampling multiple rays in a single pixel

---
+++ RayTracer/utils/common.h
@@ -0,0 +1,44 @@
+#ifndef RTCOMMON_H
+#define RTCOMMON_H
+
+#include <cmath>
+#include <random>
+#include <limits>
+#include <memory>
+
+
+// Usings
+
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+// Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility Functions
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+inline double random_double(double min = 0.0, double max = 1.0) {
+    static std::uniform_real_distribution<double> distribution(min, max);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+}
+
+// Common Headers
+
+#include "../maths/ray.h"
+#include "../maths/vec3.h"
+
+#endif
