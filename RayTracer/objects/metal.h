57c366f2103b52e7f5de02b39ef261c330239d65
Mohamed Zaki <mamadovzicx@gmail.com>
2023-05-07 04:34:29 +0300

    Added materials models

---
+++ RayTracer/objects/metal.h
@@ -0,0 +1,25 @@
+#ifndef METAL_H
+#define METAL_H
+
+#include "hittable.h"
+#include "material.h"
+
+class metal : public material {
+    public:
+        metal(const color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
+
+        virtual bool scatter(
+            const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+        ) const override {
+            vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+            scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
+            attenuation = albedo;
+            return (dot(scattered.direction(), rec.normal) > 0);
+        }
+
+    public:
+        color albedo;
+        double fuzz;
+};
+
+#endif // !METAL_H
